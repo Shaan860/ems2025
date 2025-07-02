7aec33bf9969c54c7b1edd695f9a17b51e1c1836
Shaan860 <shaan860@gmail.com>
2025-07-02 13:04:40 +0530

    secont commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { AddEmpComponent } from './components/add-emp/add-emp.component';
 import { EmpListComponent } from './components/add-emp/emp-list/emp-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
     {
         path:'add-emp',
         component: AddEmpComponent
